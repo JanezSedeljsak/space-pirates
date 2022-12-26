53e2c85efaaf76d3ab17396932fcf2616b4d0baa
Yxmaxy <57218072+Yxmaxy@users.noreply.github.com>
2022-12-26 12:32:35 +0100

    Added scoreboard

---
+++ src/gui/menus.js
@@ -0,0 +1,9 @@
+const scoreboard = document.getElementById("scoreboard");
+
+function hideScoreboard() {
+    scoreboard.style.display = "none";
+}
+
+function showScoreboard() {
+    scoreboard.style.display = "block";
+}

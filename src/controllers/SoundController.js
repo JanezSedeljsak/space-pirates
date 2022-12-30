ddc0657b9dbd1461b94ceafac6273b2c59846d2e
Yxmaxy <57218072+Yxmaxy@users.noreply.github.com>
2022-12-30 11:02:59 +0100

    Sound starts on first click

--- src/controllers/SoundController.js
+++ src/controllers/SoundController.js
@@ -1,18 +1,28 @@
 export class SoundController {
     constructor(gameController) {
-        // this.gameController = gameController;
         this.soundControl = document.getElementById("soundControl");
         this.ambientSound = document.getElementById("ambientSound");
+        this.gameController = gameController;
 
-        // const { soundVolume } = this.gameController.state;
+        const { soundVolume } = this.gameController.state;
         this.soundControl.addEventListener('change', () => this.changeSoundVolume());
-        // this.soundControl.value = 0.5;
-        // this.ambientSound.volume = 0.5;
+        if (soundVolume) {
+            this.setVolume(soundVolume);
+        }
+
+        // music will not autoplay without user interaction >:(
+        document.addEventListener('click', () => {
+            this.ambientSound.play();
+        }, { once: true });
+    }
+
+    setVolume(volume) {
+        this.soundControl.value = volume;
+        this.changeSoundVolume();
     }
 
     changeSoundVolume() {
-        // console.log(this.ambientSound.volume, this.soundControl.value);
         this.ambientSound.volume = this.soundControl.value;
-        // this.gameController.setState({ soundVolume: this.soundControl.value })
+        this.gameController.setState({ soundVolume: this.soundControl.value })
     }
 }
